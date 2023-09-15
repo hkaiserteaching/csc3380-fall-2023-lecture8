3ea6c22997c4e89f8b6e9120b76c6593d9d9cbba
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2023-09-15 11:41:52 -0500

    Adding instrumented tests

--- code/instrumented.hpp
+++ code/instrumented.hpp
@@ -1,18 +1,24 @@
+#pragma once
 
 #include <concepts>
 
 struct instrumented_base
 {
-    enum operations {
-        copy, assignment, destructor, default_constructor,
-        equality, comparison, construction
+    enum operations
+    {
+        n = 0,
+        copy,
+        assignment,
+        destructor,
+        default_constructor,
+        equality,
+        comparison,
+        construction
     };
 
-    static constexpr size_t number_ops = 7;
-    static constexpr char const* counter_names[number_ops] = {
-        "copy", "assignment", "destructor", "default_constructor",
-        "equality", "comparison", "construction"
-    };
+    static constexpr size_t number_ops = 8;
+
+    static char const* counter_names[number_ops];
     static double counts[number_ops];
 
     static void initialize(size_t);
@@ -22,5 +28,67 @@ template <typename T>
     requires(std::semiregular<T> || std::regular<T> || std::totally_ordered<T>)
 struct instrumented final : instrumented_base
 {
-    // ...
+    using value_type = T;
+
+    T value;
+
+    // Conversions from T and to T:
+    instrumented(T const& x)
+      : value(x)
+    {
+        ++counts[construction];
+    }
+
+    // Semiregular
+    instrumented()
+    {
+        ++counts[default_constructor];
+    }
+    ~instrumented()
+    {
+        ++counts[destructor];
+    }
+
+    instrumented(instrumented const& x)
+      : value(x.value)
+    {
+        ++counts[copy];
+    }
+
+    instrumented& operator=(instrumented const& x)
+    {
+        ++counts[assignment];
+        value = x.value;
+        return *this;
+    }
+
+    // Regular
+    friend bool operator==(instrumented const& x, instrumented const& y)
+    {
+        ++counts[equality];
+        return x.value == y.value;
+    }
+    friend bool operator!=(instrumented const& x, instrumented const& y)
+    {
+        return !(x == y);
+    }
+
+    // TotallyOrdered
+    friend bool operator<(instrumented const& x, instrumented const& y)
+    {
+        ++counts[comparison];
+        return x.value < y.value;
+    }
+    friend bool operator>(instrumented const& x, instrumented const& y)
+    {
+        return y < x;
+    }
+    friend bool operator<=(instrumented const& x, instrumented const& y)
+    {
+        return !(y < x);
+    }
+    friend bool operator>=(instrumented const& x, instrumented const& y)
+    {
+        return !(x < y);
+    }
 };
